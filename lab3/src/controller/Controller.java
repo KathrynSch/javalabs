dcc8afb2de9989623a9df503765db4f4b45fbf3a
Kathryn Schutte <kathryn.schutte@gmail.com>
2018-01-28 15:20:26 +0100

    lab3 final

--- lab3/src/controller/Controller.java
+++ lab3/src/controller/Controller.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Set;
 
 import javax.swing.JTextField;
 import javax.swing.text.JTextComponent;
@@ -13,20 +14,25 @@ import view.GUIListener;
 public class Controller implements GUIListener{
 
 	private UserList model;
-	private View view;
+	private Set<View> viewSet;
 	/*
 	 * controller has a reference to a UserList and not a Model because it has to update the model (write)
 	 * has a reference to a View, which is an interface because it only needs to read the data from the view
 	 */
-	public Controller(UserList model, View view) {
+	public Controller(UserList model, Set<View> viewSet) {
 		this.model = model;
-		this.view = view;
-		view.setController(this);
-		this.model.addObserver(view);
+		this.viewSet = viewSet;
+		for(View view : viewSet) {
+			view.setController(this);
+			this.model.addObserver(view);
+		}
+		
 	}
 	
 	public void start() {
-		view.start();
+		for(View view : viewSet) {
+			view.start();
+		}
 	}
 
 	@Override
@@ -37,15 +43,8 @@ public class Controller implements GUIListener{
                 model.addUser(textComponent.getText());
             }
         });
-		System.out.println(textComponent.getActionListeners().length);
 		return textComponent.getActionListeners()[0];
 	}
 	
-//	@Override
-//    public ActionListener getAddUserListener(JTextComponent textComponent) {
-//        return (ActionEvent ae) -> {
-//            model.addUser(textComponent.getText());
-//        };
-//    }
 
 }
--- lab3/src/view/CLI.java
+++ lab3/src/view/CLI.java
@@ -1,45 +1,58 @@
-//package view;
-//
-//import javax.swing.*;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Observable;
-//
-///**
-// * @author Frederic Fauberteau
-// */
-//public class CLI /* TODO: make this class a View */ {
-//
-//    public void update(Observable o, Object arg) {
-//        new SwingWorker<List<String>,String>() {
-//
-//            List<String> users = new ArrayList<>();
-//
-//            @Override
-//            protected List<String> doInBackground() throws Exception {
-//                String[] data; // TODO: get data
-//                int index = 0;
-//                while (index < data.length && ! isCancelled()) {
-//                    users.add(data[index]);
-//                    publish((data[index]));
-//                    index++;
-//                }
-//                return users;
-//            }
-//
-//            @Override
-//            protected void process(List<String> chunks) {
-//                for (String data : chunks) {
-//                    System.out.println(data);
-//                }
-//            }
-//
-//            @Override
-//            protected void done() {
-//                System.out.println("---");
-//            }
-//
-//        }.execute();
-//    }
-//
-//}
+package view;
+
+import javax.swing.*;
+
+import model.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+/**
+ * @author Frederic Fauberteau
+ */
+public class CLI extends AbstractView {
+
+    public CLI(Model model) {
+		super(model);
+	}
+
+	public void update(Observable o, Object arg) {
+        new SwingWorker<List<String>,String>() {
+
+            List<String> users = new ArrayList<>();
+
+            @Override
+            protected List<String> doInBackground() throws Exception {
+                String[] data = getModel().getData();
+                int index = 0;
+                while (index < data.length && ! isCancelled()) {
+                    users.add(data[index]);
+                    publish((data[index]));
+                    index++;
+                }
+                return users;
+            }
+
+            @Override
+            protected void process(List<String> chunks) {
+                for (String data : chunks) {
+                    System.out.println(data);
+                }
+            }
+
+            @Override
+            protected void done() {
+                System.out.println("---");
+            }
+
+        }.execute();
+    }
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
